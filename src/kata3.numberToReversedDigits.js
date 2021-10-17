0a607b3e01922e34b9dfa2685d3c4fed94481cc2
MaggieTheCoder <mdurandt@gmail.com>
2021-10-17 20:29:51 +0100

    solved humancatdogyears

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,3 +1,4 @@
-const numberToReversedDigits = number => {};
-
+const numberToReversedDigits = number => {
+  return Array.from(String(number), Number).reverse();
+};
 module.exports = numberToReversedDigits;
--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,19 @@
-const humanCatDogYears = number => {};
+const humanCatDogYears = number => {
+  let catYears = 0;
+  let dogYears = 0;
+  if (number === 1) {
+    catYears = 15;
+    dogYears = 15;
+  } else if (number === 2) {
+    catYears = 24;
+    dogYears = 24;
+  } else if (number >= 3) {
+     //number - 2 to get rid of first two years//
+     // if number = 3; catYears = 28, 
+    catYears = 4 * (number - 2) + 24;
+    dogYears = 5 * (number - 2) + 24;
+  }
+  return [number, catYears, dogYears];
+};
 
 module.exports = humanCatDogYears;
